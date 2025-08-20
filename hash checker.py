56d8e1c5ec2fbb8bf683a7601ba0c89178f16673
sanju464 <sanjuthomas459@gmail.com>
2025-08-20 22:34:52 +0530

    Add files via upload
    this is the first version creates a hash of a websites and writes it into a file

---
+++ hash checker.py
@@ -0,0 +1,40 @@
+import requests
+import hashlib
+from bs4 import BeautifulSoup
+
+# Website you want to check
+URL = "https://time.is/"
+
+def get_page_hash(url):
+    response = requests.get(url)          # download the webpage
+    soup = BeautifulSoup(response.text, "html.parser")  
+    content = soup.get_text()             # extract only text (ignore HTML tags)
+    return hashlib.md5(content.encode("utf-8")).hexdigest()
+#collect curent and present hash
+
+
+
+
+hash1=get_page_hash(URL)
+# Open a file in write mode ("w")
+file = open("hash.txt", "w")
+
+# Write text into it
+file.write(hash1)
+# Always close the file to save changes
+file.close()
+
+
+print("website:",URL)
+print("hash:",hash1)
+print("hash2:")
+
+# if hash1==hash2:
+#     print("no change")
+#     #print("hash:",hash1)
+# else:
+#     print("change detected")
+    #print("hash1:",hash1)
+    #print("hash2:",hash2)
+#print("Website:", URL)
+#print("Hash value:", page_hash)
